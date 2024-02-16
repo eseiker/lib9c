8236bb0caf9b4fa5edb3df7f1df36bfa92b8bb36
area363 <kidonseo@gmail.com>
2024-02-16 13:00:13 +0900

    enable 0 quota

--- Lib9c.Policy/NCStagePolicy.cs
+++ Lib9c.Policy/NCStagePolicy.cs
@@ -20,7 +20,7 @@ namespace Nekoyume.Blockchain
 
         public NCStagePolicy(TimeSpan txLifeTime, int quotaPerSigner, IAccessControlService? accessControlService = null)
         {
-            if (quotaPerSigner < 1)
+            if (quotaPerSigner < 0)
             {
                 throw new ArgumentOutOfRangeException(
                     $"{nameof(quotaPerSigner)} must be positive: ${quotaPerSigner}");
